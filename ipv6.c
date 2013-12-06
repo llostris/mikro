6de38145f828e7396779f80d1e2e5dab3b583624
llostris <deiidrell@gmail.com>
2013-12-06 04:37:51 -0500

    small checksum_pseudo fix

--- ipv6.c
+++ ipv6.c
@@ -49,7 +49,7 @@ uint16_t checksum(void* data, uint16_t length) {
 // might not work for not-ndp packets
 /* ipv6 pseudo-header used to calculate the checksum contains source address, destination address, icmpv6 length, 3 octets of 0 and next header id */
 uint16_t checksum_pseudo(void* data, uint16_t* src_addr, uint16_t* dest_addr, int next_hdr, uint8_t data_len) {
-	uint16_t pseudo_hdr_len = ICMP_HDR_LEN + 4 * IPV6_ADDR_LEN + 8 + ICMP_NDP_LEN;
+	uint16_t pseudo_hdr_len = 4 * IPV6_ADDR_LEN + 8 + data_len;
 	uint8_t buf[pseudo_hdr_len];
 	memset(buf, 0x0, pseudo_hdr_len);
 	memcpy(buf, src_addr, IPV6_ADDR_LEN * 2);
